dd97056421ef1f1cc0e982b05bd24622c42b0b01
igordmouraa <igordmoura_@hotmail.com>
2024-11-21 11:19:05 -0300

    feat: Add initial Spring Boot application with basic test
    This commit introduces the initial setup of the Spring Boot application, including the main application class and a basic test case to ensure the application context loads. This provides the foundation for building and testing further functionalities in the project.

---
+++ src/test/java/br/com/igordmoura/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igordmoura.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
