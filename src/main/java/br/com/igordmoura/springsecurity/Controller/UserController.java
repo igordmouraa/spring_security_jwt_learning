93ffccca639ef82ad00b2098d9a44722fd0f0ca7
igordmouraa <igordmoura_@hotmail.com>
2024-11-21 16:09:05 -0300

    feat: Add UserController to handle user creation
    Introduces a new `UserController` class with a `/users` endpoint to manage user creation. The controller checks for existing users, encrypts passwords, assigns a basic role, and saves new users to the database.

---
+++ src/main/java/br/com/igordmoura/springsecurity/Controller/UserController.java
@@ -0,0 +1,52 @@
+package br.com.igordmoura.springsecurity.Controller;
+
+import br.com.igordmoura.springsecurity.Controller.dto.CreateUserDto;
+import br.com.igordmoura.springsecurity.Entities.Role;
+import br.com.igordmoura.springsecurity.Entities.User;
+import br.com.igordmoura.springsecurity.Repositories.RoleRepository;
+import br.com.igordmoura.springsecurity.Repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/users")
+    @Transactional
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto) {
+
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userFromDb = userRepository.findByUsername(dto.username());
+
+        if(userFromDb.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "User already exists");
+        }
+
+        var user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+}
